eccfae6344bf0da310781396581a07c69962b45d
huahua888 <65008463+huahua888@users.noreply.github.com>
2020-05-14 19:01:45 +0800

    Add files via upload

---
+++ 学生信息管理系统/StudentsInfo/teacher_index.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Model;
+
+namespace 学生信息管理系统.StudentsInfo
+{
+    public partial class teacher_index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lb_user.Text = ((Model.Login)Session["CurrentUser"]).StuName.ToString();
+            lb_time.Text = DateTime.Now.ToString();
+        }
+    }
+}
